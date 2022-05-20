9b7762f9ea28f446f2811dd0dae9a9c2ec10d182
Zicheng.S <zichengshan@gmail.com>
2022-05-20 11:13:51 -0700

    Frontend setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import {ChatEngine} from "react-chat-engine"
+
+import './App.css'
+
+function App() {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID=""
+            userName=""
+            userSecret=""
+        />
+    )
+}
+
+export default App
